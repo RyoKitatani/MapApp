29085155fd43badef1629d6e4db921208253216f
RyoKitatani <ryo0929m@gmail.com>
2021-05-12 09:06:12 +0900

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by RyoKitatani on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
